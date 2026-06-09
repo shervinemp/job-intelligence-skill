6981186edd0c70c1eaaa0712ed22917de92b1602
Shervin Naseri <shervin.naseri@gmail.com>
2026-06-09 19:46:07 -0400

    fix: SNAPSHOTS_DIR must be Path not str

--- job_intelligence/lib/config.py
+++ job_intelligence/lib/config.py
@@ -5,12 +5,13 @@ Default: ~/.ji/
 """
 
 import os
+from pathlib import Path
 
 JI_HOME = os.environ.get("JI_HOME", os.path.join(os.path.expanduser("~"), ".ji"))
 
 STATE_DIR = os.path.join(JI_HOME, "state")
 RESULTS_DIR = os.path.join(JI_HOME, "results")
-SNAPSHOTS_DIR = os.path.join(JI_HOME, "snapshots")
+SNAPSHOTS_DIR = Path(JI_HOME) / "snapshots"
 CHROME_PROFILE = os.path.join(JI_HOME, "chrome-profile")
 
 DB_PATH = os.path.join(STATE_DIR, "jobs.db")
